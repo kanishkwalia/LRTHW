e47914cf6cbbb9ce21536aad5560f5b597517352
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-08 18:00:00 +0000

    ex1 added

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "I am printing another line"
+# Adding an octothorpe at the beginning of a line to see what it does
